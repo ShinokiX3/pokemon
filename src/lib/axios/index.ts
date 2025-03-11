22029163228a2c8306e9053f644bb283fdd174df
ShinokiX3 <endernasha85@gmail.com>
2025-03-11 15:54:35 +0200

    add: axios instance

---
+++ src/lib/axios/index.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+const BASE_URL = 'https://pokeapi.co/api/v2';
+
+export const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+});
